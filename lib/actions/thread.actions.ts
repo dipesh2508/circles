40d62f0b231a258b38b77b2d531e137d0ef7629a
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-12-03 02:15:41 +0530

    created fetch posts thread action

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -39,3 +39,35 @@ export async function createThread({
     throw new Error("Failed to create thread" + err.message);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  //Calculate the skip value
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  //Fetch the posts that have no parents (top level posts)
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
