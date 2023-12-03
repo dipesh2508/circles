c16ee16ad44f8762530a2eeaf15be0a3d1a272f9
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2023-12-04 03:20:41 +0530

    added Thread Card

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import Image from "next/image";
+
+type Props = {
+  id: string;
+  currentUserId: string;
+  parentId: string | null;
+  content: string;
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  community: {
+    name: string;
+    image: string;
+    id: string;
+  } | null;
+  createdAt: string;
+  comments: {
+    author: {
+      image: string;
+    };
+  }[];
+  isComment?: boolean;
+};
+
+const ThreadCard = ({
+  id,
+  currentUserId,
+  parentId,
+  content,
+  author,
+  community,
+  createdAt,
+  comments,
+}: Props) => {
+  return (
+    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+              <Image
+                src={author.image}
+                alt="profile picture"
+                fill
+                className="cursor-pointer rounded-full"
+              />
+            </Link>
+
+            <div className="thread-card_bar" />
+          </div>
+          <div className="flex w-full flex-col">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-base-semibold text-light-1">
+                {author.name}
+              </h4>
+            </Link>
+
+            <p className="mt-2 text-small-regular text-light-2">{content}</p>
+            <div className="mt-5 flex flex-col gap-3">
+              {/* continue from here */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default ThreadCard;
