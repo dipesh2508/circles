b0b3331af1ae0d36a05444621e7b8381964f4e72
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-01-04 20:13:43 +0530

    added activity user action

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import UserCard from "@/components/cards/UserCard";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
 const page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) return redirect("/onboarding");
+
+  
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -127,3 +127,28 @@ export async function fetchUsers({
     throw new Error("Failed to fetch users" + e);
   }
 }
+
+
+export async function getActivity(userId: string){
+  try{
+    connectToDB();
+
+    const userThreads = await Thread.find({author: userId});
+
+    const childThreadIds = userThreads.reduce((acc, userThread) =>{
+      return acc.concat(userThread.children);
+    })
+
+    const replies = await Thread.find({
+      _id: {$in: childThreadIds},
+      author: {$ne: userId}
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name image _id'
+    });
+
+  } catch (e: any) {
+    throw new Error("Failed to fetch activity" + e);
+  }
+}
