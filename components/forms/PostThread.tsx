27de6ce1065357719160bea44a47088e50bad09a
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-01-05 21:06:51 +0530

    added organization details

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -17,6 +17,8 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { usePathname, useRouter } from "next/navigation";
 
+import { useOrganization } from "@clerk/nextjs";
+
 // import { updateUser } from "@/lib/actions/user.actions";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
@@ -35,6 +37,8 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
+
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
     defaultValues: {
@@ -47,7 +51,7 @@ function PostThread({ userId }: { userId: string }) {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
